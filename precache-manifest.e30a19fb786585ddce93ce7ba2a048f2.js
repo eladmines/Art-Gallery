2548e6fc72954724d52cb78343cb3a18f40fcb44
eladmines <mineselad@gmail.com>
2020-07-01 12:54:35 +0300

    Updates

---
+++ precache-manifest.e30a19fb786585ddce93ce7ba2a048f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bf9d13ca86efc8f1b83e1280355db43",
+    "url": "/Art-Gallery/index.html"
+  },
+  {
+    "revision": "d657bb58ab9f22bff45d",
+    "url": "/Art-Gallery/static/css/2.6c107856.chunk.css"
+  },
+  {
+    "revision": "6fdefdcbaa09f6b76579",
+    "url": "/Art-Gallery/static/css/main.72e07da2.chunk.css"
+  },
+  {
+    "revision": "d657bb58ab9f22bff45d",
+    "url": "/Art-Gallery/static/js/2.fba5d8f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Art-Gallery/static/js/2.fba5d8f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fdefdcbaa09f6b76579",
+    "url": "/Art-Gallery/static/js/main.df186cf1.chunk.js"
+  },
+  {
+    "revision": "28217db0d360fbaf49bd",
+    "url": "/Art-Gallery/static/js/runtime-main.bc7b8c62.js"
+  }
+]);
